abce55918b75cd7edd5429a62a0b0d756112c027
IgorP25 <igor@zimp.com.au>
2025-02-24 03:24:15 +1100

    Refactored maze settings to separate variables

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from window import Window
 from maze import Maze
+from window import Window
+import sys
 
 def main():
-    win = Window(800, 600)
-    m1 = Maze(10, 10, 19, 26, 30, 30, win)
+    seed = None
+    num_rows = 19
+    num_cols = 26
+    margin = 10
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
+    m1 = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, seed=seed)
     if not m1.solve():
         print("Maze cannot be solved. Try again.")
     win.wait_for_close()
