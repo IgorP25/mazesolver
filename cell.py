90ef5994ae0d8fc6926c4730d35eef34ecf1361a
IgorP25 <igor@zimp.com.au>
2025-02-24 02:47:00 +1100

    Moved xy coords from Cell constructor to draw method

--- cell.py
+++ cell.py
@@ -2,38 +2,54 @@ from point import Point
 from line import Line
 
 class Cell():
-    def __init__(self, x1, y1, x2, y2, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+        self.visited = False
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        self._win = win
-
-    def draw(self):
+        p1 = Point(x1, y1)
+        p2 = Point(x1, y2)
+        l1 = Line(p1, p2)
+        color = "white"
         if self.has_left_wall:
-            p1 = Point(self._x1, self._y1)
-            p2 = Point(self._x1, self._y2)
-            l1 = Line(p1, p2)
-            self._win.draw_line(l1, "black")
+            color = "black"
+        self._win.draw_line(l1, color)
+        
+        p1 = Point(x2, y1)
+        p2 = Point(x2, y2)
+        l1 = Line(p1, p2)
+        color = "white"
         if self.has_right_wall:
-            p1 = Point(self._x2, self._y1)
-            p2 = Point(self._x2, self._y2)
-            l1 = Line(p1, p2)
-            self._win.draw_line(l1, "black")
+            color = "black"
+        self._win.draw_line(l1, color)
+        p1 = Point(x1, y1)
+        p2 = Point(x2, y1)
+        l1 = Line(p1, p2)
+        color = "white"
         if self.has_top_wall:
-            p1 = Point(self._x1, self._y1)
-            p2 = Point(self._x2, self._y1)
-            l1 = Line(p1, p2)
-            self._win.draw_line(l1, "black")
+            color = "black"
+        self._win.draw_line(l1, color)
+        p1 = Point(x1, y2)
+        p2 = Point(x2, y2)
+        l1 = Line(p1, p2)
+        color = "white"
         if self.has_bottom_wall:
-            p1 = Point(self._x1, self._y2)
-            p2 = Point(self._x2, self._y2)
-            l1 = Line(p1, p2)
-            self._win.draw_line(l1, "black")
+            color = "black"
+        self._win.draw_line(l1, color)
 
     def draw_move(self, to_cell, undo=False):
         p1 = Point(self._x1 + ((self._x2 - self._x1) / 2), self._y1 + ((self._y2 - self._y1) / 2))
