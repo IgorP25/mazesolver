b5bd5f6a85873226151d68509e3c37eab3512fd3
IgorP25 <igor@zimp.com.au>
2025-02-24 03:15:59 +1100

    Added check if maze is unsolvable

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from maze import Maze
 def main():
     win = Window(800, 600)
     m1 = Maze(10, 10, 19, 26, 30, 30, win)
-    m1.solve()
+    if not m1.solve():
+        print("Maze cannot be solved. Try again.")
     win.wait_for_close()
 
 main()
